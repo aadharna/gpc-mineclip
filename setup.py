729778c029b1fd42173f7728daef2a7c25b13a3a
aadharna <aadharna@gmail.com>
2022-11-09 14:54:00 -0800

    add setup.py file. add mineclip to packages

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gpc-mineclip',
+    version='0.1',
+    packages=['mineclip'],
+    url='git@github.com:aadharna/gpc-mineclip.git',
+    license='MIT',
+    author='Aaron Dharna, Ruiyu Gou, Nicholas Ioannidis',
+    author_email='',
+    description=''
+)
