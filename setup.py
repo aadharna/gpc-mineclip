a96009a992a1e88c2fe7c13d39bee4bbea6893bd
Aaron Dharna <aadharna@gmail.com>
2022-11-10 00:26:03 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='gpc-mineclip',
     version='0.1',
-    packages=[''],
+    packages=[],
     url='git@github.com:aadharna/gpc-mineclip.git',
     license='MIT',
     author='Aaron Dharna, Ruiyu Gou, Nicholas Ioannidis',
