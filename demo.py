47cd146b9b5dcdf90c9835b6f2d12acb31e921f0
nickioan <nicholasioannidis98@gmail.com>
2022-11-19 16:28:41 -0800

    added suggestions discussed

--- demo.py
+++ demo.py
@@ -1,22 +1,25 @@
 import minedojo
-from wrappers import MinedojoWrapper
+from wrappers import MineClipWrapper
 
 
 def run():
     env = minedojo.make(
         task_id="combat_spider_plains_leather_armors_diamond_sword_shield",
-        image_size=(288, 512),
+        image_size=(160, 256),
         world_seed=123,
         seed=42,
     )
     prompts = [
+        "find spider",
         "kill spider",
     ]
-    env = MinedojoWrapper(env,prompts)
-    env.reset()
-    for _ in range(1):
-        obs, reward, done, info = env.step(env.action_space.no_op())
-        print(reward)
+    env = MineClipWrapper(env,prompts)
+    for episode in range(2):
+        env.reset()
+        done = False
+        while not done:
+            obs, reward, done, info = env.step(env.action_space.no_op())
+        env.change_prompt()
     env.close()
 
 if __name__ == "__main__":
