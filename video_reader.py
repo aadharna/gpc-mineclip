b31858a42b3ea67773ba174abed75652faae39a0
aadharna <aadharna@gmail.com>
2022-12-06 21:10:32 -0800

    add all tasks into conf and make video_reader file work off commandline

--- video_reader.py
+++ video_reader.py
@@ -1,42 +1,52 @@
+import os
 import torch
 import torchvision
 from torch.utils.data import DataLoader
 import wandb
 import yaml
-from video_utils import VideoDataset,init_wandb,load_mineclip,example_read_video
-
+from video_utils import (VideoDataset,
+                         init_wandb,
+                         load_mineclip,
+                         example_read_video)
 
 STREAM = 'video'
-TASK = 'task1'
 
-with open("tasks_conf.yaml", "r") as stream:
-    try:
-        conf = yaml.safe_load(stream)[TASK]
-    except yaml.YAMLError as exc:
-        print(exc)
+if __name__ == '__main__':
+    import argparse
+    # get the task name from the command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--task', type=str, default='mineclip')
+    args = parser.parse_args()
+    TASK = args.task
 
-PATH = '../minecraft_science/{}.mp4'.format(conf['name'])
-prompts = conf['prompts']
+    with open("tasks_conf.yaml", "r") as stream:
+        try:
+            conf = yaml.safe_load(stream)[TASK]
+        except yaml.YAMLError as exc:
+            print(exc)
 
-init_wandb(task_id=conf['name'])
-prompt_ids = ["Sub-Task {}".format(x+1) for x in range(len(prompts))]
+    PATH = os.path.join(os.getcwd(), 'recordings', '{}.mp4'.format(conf['name']))
+    prompts = conf['prompts']
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-video = torchvision.io.VideoReader(PATH, STREAM)
-vf,info, meta = example_read_video(video)
-dataset = VideoDataset(vf)
+    init_wandb(task_id=conf['name'])
+    prompt_ids = ["Sub-Task {}".format(x+1) for x in range(len(prompts))]
 
-loader = DataLoader(
-    dataset,
-    batch_size=16,
-    shuffle=False
-)
-model = load_mineclip(device)
-with torch.no_grad():
-    prompt_feats = model.encode_text(prompts)
-for data in loader:
-    data = torch.unsqueeze(data,dim=0).to(device)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    video = torchvision.io.VideoReader(PATH, STREAM)
+    vf, info, meta = example_read_video(video)
+    dataset = VideoDataset(vf)
+
+    loader = DataLoader(
+        dataset,
+        batch_size=16,
+        shuffle=False
+    )
+    model = load_mineclip(device)
     with torch.no_grad():
-        reward,_ = model(data, text_tokens=prompt_feats, is_video_features=False)
-    log_data = dict(zip(prompt_ids, reward[0].cpu().numpy()))
-    wandb.log(log_data)
+        prompt_feats = model.encode_text(prompts)
+    for data in loader:
+        data = torch.unsqueeze(data, dim=0).to(device)
+        with torch.no_grad():
+            reward, _ = model(data, text_tokens=prompt_feats, is_video_features=False)
+        log_data = dict(zip(prompt_ids, reward[0].cpu().numpy()))
+        wandb.log(log_data)
