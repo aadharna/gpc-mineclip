63d0c9dbac55d38a123634b1a7f71e5e9b7a833c
nickioan <nicholasioannidis98@gmail.com>
2022-12-16 22:41:15 -0800

    fixed loss function for SI and added threshold for good rollouts

--- buffer.py
+++ buffer.py
@@ -7,8 +7,10 @@ Rollout = namedtuple('Rollout',
 
 class ReplayMemory(object):
 
-    def __init__(self, capacity):
+    def __init__(self, capacity,si_counter = 1):
         self.memory = deque([],maxlen=capacity)
+        self.si_counter = si_counter
+        self.average_reward = 0
 
     def push(self, *args):
         """Save a transition"""
